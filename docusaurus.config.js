1238fe2010fb12b57734f434942005dfef65b005
christian-herrera <chris1496.rp@gmail.com>
2025-11-14 10:42:29 -0300

    :sparkles: agrega apuntes sobre PHP

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -138,6 +138,7 @@ const config = {
             },
             prism: {
                 theme: prismThemes.gruvboxMaterialDark,
+                additionalLanguages: ["php", "bash"],
             },
         }),
 };
